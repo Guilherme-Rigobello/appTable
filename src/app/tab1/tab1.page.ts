7cd9c4398601327eb99a6801d3d4b9b84458868d
Laboratório FIAP <labsfiap@fiap.com.br>
2025-06-02 13:40:22 -0300

    emprestimo-pagina

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss'],
+  standalone: false,
+})
+export class Tab1Page {
+  constructor() {}
+
+  alertButtons = ['OK'];
+
+  meses = 0;
+  taxa = 0;
+  valor = 0;
+  valorParcela = 0;
+  valorArredondado = ''
+  res = '';
+
+  calculoParcela() {
+    this.valorParcela =
+    (this.valor * this.taxa) / 1 - (1 + this.taxa) ** (-1 * this.meses);
+
+    this.valorArredondado = this.valorParcela.toFixed(2)
+
+    if (this.taxa > 20) {
+      this.res = `Seu empréstimo é arriscado! `;
+    }
+    else (this.res = `Faça uma avaliação mais cuidadosa!`);
+  }
+}
